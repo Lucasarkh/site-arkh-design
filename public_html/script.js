7ee05be3f275625442d9b1a274c980435535ee6d
Lucasarkh <lucasarchanjo1010@gmail.com>
2023-02-08 00:02:18 -0300

    Inserido botão de menu escondido

---
+++ public_html/script.js
@@ -0,0 +1,13 @@
+const btnMobile = document.getElementById('btn-menu');
+
+function toggleMenu() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+
+}
+
+
+btnMobile.addEventListener('click', toggleMenu);
+
+
+
